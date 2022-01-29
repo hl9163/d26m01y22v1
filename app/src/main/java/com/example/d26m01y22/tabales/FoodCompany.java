faa8b86fd6e66a72495c92df8c6f6d3bd891e116
HAREL <hl9163@bs.amalnet.k12.il>
2022-01-29 19:32:19 +0200

    finish Food Company

--- app/src/main/java/com/example/d26m01y22/tabales/FoodCompany.java
+++ app/src/main/java/com/example/d26m01y22/tabales/FoodCompany.java
@@ -3,9 +3,9 @@ package com.example.d26m01y22.tabales;
 public class FoodCompany {
 
     public static final String TABLE_FOOD_COMPANY = "FoodCompany";
-    public static final String KEY_ID = "_id";
-    public static final String NUMBER="NUMBER";
-    public static final String NAME="Name";
-    public static final String FIRST_PHONE_NUMBER ="FIRST_PHONE_NUMBER";
-    public static final String SECOND_PHONE_NUMBER = "SECOND_PHONE_NUMBER";
+    public static final String KEY_ID_Food = "_id";
+    public static final String COMPANY_NUMBER="COMPANY_NUMBER";
+    public static final String COMPANY_NAME ="COMPANY_NAME";
+    public static final String C_FIRST_PHONE_NUMBER ="C_FIRST_PHONE_NUMBER";
+    public static final String C_SECOND_PHONE_NUMBER = "C_SECOND_PHONE_NUMBER";
 }
