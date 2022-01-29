2f2119b7d4a11a3bd686eaccb93f56dc0217039f
HAREL <hl9163@bs.amalnet.k12.il>
2022-01-29 22:19:07 +0200

    finish food company

--- app/src/main/java/com/example/d26m01y22/tabales/FoodCompany.java
+++ app/src/main/java/com/example/d26m01y22/tabales/FoodCompany.java
@@ -8,4 +8,5 @@ public class FoodCompany {
     public static final String COMPANY_NAME ="COMPANY_NAME";
     public static final String C_FIRST_PHONE_NUMBER ="C_FIRST_PHONE_NUMBER";
     public static final String C_SECOND_PHONE_NUMBER = "C_SECOND_PHONE_NUMBER";
+    public static final String IS_WORKING_COMPANY = "IS_WORKING";
 }
