1da14b2f107106a1c64b7f196311c586d072cc0b
HAREL <hl9163@bs.amalnet.k12.il>
2022-01-30 22:30:03 +0200

    finish Food Company table

--- app/src/main/java/com/example/d26m01y22/tabales/FoodCompany.java
+++ app/src/main/java/com/example/d26m01y22/tabales/FoodCompany.java
@@ -3,7 +3,7 @@ package com.example.d26m01y22.tabales;
 public class FoodCompany {
 
     public static final String TABLE_FOOD_COMPANY = "FoodCompany";
-    public static final String KEY_ID_Food = "_id";
+    public static final String KEY_ID_FoodC = "_id";
     public static final String COMPANY_NUMBER="COMPANY_NUMBER";
     public static final String COMPANY_NAME ="COMPANY_NAME";
     public static final String C_FIRST_PHONE_NUMBER ="C_FIRST_PHONE_NUMBER";
