7aac82f114a3a73b9afe2cb996f954cff88a1060
HAREL <hl9163@bs.amalnet.k12.il>
2022-02-21 11:11:53 +0200

    finish project !!!!!

--- app/src/main/java/com/example/d26m01y22/creditsActivity.java
+++ app/src/main/java/com/example/d26m01y22/creditsActivity.java
@@ -1,8 +1,12 @@
 package com.example.d26m01y22;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class creditsActivity extends AppCompatActivity {
@@ -16,6 +20,36 @@ public class creditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+    }
+    /**
+     * create the menu.
+     * <p>
+     *
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+    /**
+     * move to the credits activity.
+     * <p>
+     *
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.return_mainMenu){
+            Intent si = new Intent(this,MainActivity.class);
+            startActivity(si);
+            return true;
+        }else if(id == R.id.credits){
+            Intent si = new Intent(this,creditsActivity.class);
+            startActivity(si);
+            return true;
+
+        }
+        return true;
     }
     /**
      * return the the first activity.
